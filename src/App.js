9b0d818115bcd87b06c79c26e8e97bfd870fea33
Shubham Tiwari <tshubham9648@gmail.com>
2022-04-21 11:19:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
-
+import Title from './Title';
 function App() {
+  const [movie,setMovie] = React.useState("baahubali")
+  const [id,setId] = React.useState()
+   const [data,setData] = React.useState([])
+  async function handleClick (){
+   let res = await axios.get(`https://imdb-api.com/en/API/SearchMovie/k_auitn2qs/${movie}`)
+    //  console.log(res.data.results.id)
+    
+    setData( res.data.results)
+    // console.log(data.id)
+    // setId((elem)=>{
+      
+    // })
+  }
+  // handleClick()
+  // console.log(data)
+  // console.log(id)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Title /> */}
+     <div className="header-wrapper">
+     <h1>Getting Movies and shows</h1>
+     </div>
+     <div className='main-wrapper'>
+     <input type="text" className='form' value={movie} 
+      onChange={(event)=>{
+        setMovie(event.target.value)  
+      }}/>
+        <button type='submit' onClick={handleClick}>get Movie</button> 
+     <div className="img-wrapper"> {
+        data.map((elem)=>{
+          return(
+            <div  className="container">
+              
+              <p>{elem.description}</p> 
+              
+              <p>{elem.title}</p>
+              <img src={elem.image} width="200rem"/>
+            </div>
+          )
+        })
+      }
+      </div>
+     {/* <Title title={data.data}/> */}
+    </div>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = (props) =>{
+   console.log(props)
+    return(
+        <>
+      {}   
+    
+        </>
+    )
+}
+
+export default Title
